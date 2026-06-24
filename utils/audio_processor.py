4e39a3f18b74139c1ae7a326bfb3807ef25df1fd
yadavabhishekz <yadavabhishekz1347@gmail.com>
2026-06-25 01:32:46 +0530

    wav converter working

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,11 +1,14 @@
 import yt_dlp
-from pydub import  AudioSegment
+from pydub import AudioSegment
 import os
 
-DOWNLOAD_DIR  = 'downloads'
+DOWNLOAD_DIR = 'downloads'
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-def download_youtube_audio(url :str) ->str:
+# extract audio for yt videos
+
+
+def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
         "format": "bestaudio/best",
@@ -21,7 +24,20 @@ def download_youtube_audio(url :str) ->str:
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+        filename = ydl.prepare_filename(info).replace(
+            ".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
-print(download_youtube_audio("https://youtu.be/7HSSR1n8dgc"))
+
+data = download_youtube_audio("https://youtu.be/7HSSR1n8dgc")
+
+
+def convert_to_wav(input_path: str) -> str:
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000)
+    audio.export(output_path, format="wav")
+    return output_path
+
+
+print(convert_to_wav(data))

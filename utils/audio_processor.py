cf482c668521e3ac9dd71b6f33e1df9360519f15
yadavabhishekz <yadavabhishekz1347@gmail.com>
2026-06-24 23:52:53 +0530

    yt video audio download tested

---
+++ utils/audio_processor.py
@@ -0,0 +1,27 @@
+import yt_dlp
+from pydub import  AudioSegment
+import os
+
+DOWNLAOD_DIR  = 'downloads'
+os.makedirs(DOWNLAOD_DIR, exist_ok=True)
+
+def download_youtube_audio(url :str) ->str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+    return filename
+
+print(download_youtube_audio("https://youtu.be/7HSSR1n8dgc"))

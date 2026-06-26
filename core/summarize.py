277eb0839229cbe47e321e6e99e888b875de9f79
yadavabhishekz <yadavabhishekz1347@gmail.com>
2026-06-26 15:37:17 +0530

    feat: implement meeting insights extraction and RAG engine

--- core/summarize.py
+++ core/summarize.py
@@ -10,7 +10,7 @@ def get_llm():
     return ChatGroq(
         model="llama-3.3-70b-versatile",
         api_key=os.getenv("GROQ_API_KEY"),
-        temperature=0
+        temperature=0.3
     )
 
 def split_transcribe(transcript : str) ->list:
